25b180558ec903bc0b766ed5b0350c9d1d4bc709
伐木工人 <314313534@qq.com>
2019-06-27 19:25:50 +0800

    修复导入模型不自动释放材质和贴图的问题。

---
+++ Assets/Unity2glTF/Scripts/ForceExternalMaterialProcessor.cs
@@ -0,0 +1,31 @@
+﻿#if UNITY_EDITOR
+// An asset postprocessor that sets the material setting of a model to "Use External Materials (Legacy)".
+//
+// It only processes an asset if it's a new one, that didn't exist in the project yet.
+// Duplicating an asset inside Unity does not count as new asset in  this case.
+// It counts as new asset if the .meta file is missing.
+//
+// Save this file as: Assets/Editor/ForceExternalMaterialProcessor.cs
+//
+// Download latest version at: https://bitbucket.org/snippets/pschraut/Eea64L
+//
+using UnityEngine;
+using UnityEditor;
+
+public class ForceExternalMaterialProcessor : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+#if UNITY_2018_1_OR_NEWER
+        var importSettingsMissing = assetImporter.importSettingsMissing;
+#else
+        var importSettingsMissing = !System.IO.File.Exists(AssetDatabase.GetTextMetaFilePathFromAssetPath(assetPath));
+#endif
+        if (!importSettingsMissing)
+            return; // Asset imported already, do not process.
+
+        var modelImporter = assetImporter as ModelImporter;
+        modelImporter.materialLocation = ModelImporterMaterialLocation.External;
+    }
+}
+#endif
