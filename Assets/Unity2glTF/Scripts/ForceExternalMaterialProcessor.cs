33336dee13a24d1e38f09c907d69bf6db51be334
伐木工人 <314313534@qq.com>
2019-06-28 18:55:27 +0800

    彻底解决Linux材质无贴图问题。

--- Assets/Unity2glTF/Scripts/ForceExternalMaterialProcessor.cs
+++ Assets/Unity2glTF/Scripts/ForceExternalMaterialProcessor.cs
@@ -12,11 +12,15 @@
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System.Collections.Generic;
 
 public class ForceExternalMaterialProcessor : AssetPostprocessor
 {
+    //用来保存贴图的路径
+    static List<string> list = new List<string>();
     void OnPreprocessModel()
     {
+        list.Clear();
 #if UNITY_2018_1_OR_NEWER
         var importSettingsMissing = assetImporter.importSettingsMissing;
 #else
@@ -30,19 +34,29 @@ public class ForceExternalMaterialProcessor : AssetPostprocessor
     }
     void OnPostprocessTexture(Texture2D texture)
     {
+        list.Add(assetPath);
+
+    }
+    private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
         //资源释放顺序Win：贴图-->材质-->物体，Linux：材质-->物体-->贴图
-        //以下代码为解决Linux材质无贴图问题
-        string name = Path.GetFileNameWithoutExtension(assetPath);
-        foreach (var kv in assetImporter.GetExternalObjectMap())
+        //Linux存在材质无贴图的问题
+        //以下代码为彻底解决Linux材质无贴图问题。
+        if (importedAssets.Length > 0)
         {
-            if (kv.Key.name == name && kv.Key.type == typeof(Material))
+            foreach (string texturePath in list)
             {
-                Material mat = kv.Value as Material;
-                if (!mat.mainTexture)
+                string name = Path.GetFileNameWithoutExtension(texturePath);
+                string ext = Path.GetExtension(texturePath);
+                string matPath = texturePath.Replace("/" + name + ext, "");
+                int index = matPath.LastIndexOf("/");
+                matPath = matPath.Substring(0, index + 1);
+                matPath += "Materials/" + name + ".mat";
+                Material mat = AssetDatabase.LoadAssetAtPath<Material>(matPath);
+                if (mat && !mat.mainTexture)
                 {
-                    mat.mainTexture = texture;
+                    mat.mainTexture = AssetDatabase.LoadAssetAtPath<Texture2D>(texturePath);
                 }
-                break;
             }
         }
     }
